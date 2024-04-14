9e4c670ae92544512788925b7f5f21582fc0d5f2
Mrutunjay113 <mrutunjay765@gmail.com>
2024-04-14 14:04:27 +0530

    Add @radix-ui/react-dropdown-menu dependency and implement user account navigation in Navbar.tsx

---
+++ src/components/UserAccountNav.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { User } from "@/payload-types";
+import Link from "next/link";
+import { useAuth } from "./user-auth";
+
+const UserAccountNav = ({ user }: { user: User }) => {
+  const { signOut } = useAuth();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="overflow-visible">
+        <Button variant="ghost" size="sm" className="relative">
+          My Account
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="bg-white w-60 " align="end">
+        <div className="flex items-center justify-start gpa-2 p-2">
+          <div className="flex flex-col space-y-0.5 leading-none">
+            <p className="font-medium text-sm">{user.email}</p>
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href="/sell">Seller Dashboard</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem className="cursor-pointer" onClick={signOut}>
+          Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+export default UserAccountNav;
